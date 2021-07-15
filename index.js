4e5d8c4783cbe7579065e1dae92bb833686ae4a0
ishangupta6412 <ig.20u10045@btech.nitdgp.ac.in>
2021-07-16 02:31:51 +0530

    first

---
+++ index.js
@@ -0,0 +1,12 @@
+// $("button").click(function(){
+//     alert("hello");
+// });
+
+for(var i=0; i<7;i++){
+document.querySelectorAll("button")[i].addEventListener("click", function(){
+    alert("hello");
+});
+}
+
+
+
